6c489000fdd80c0c80015db522c8aea1768a7eb5
Kenlnw <sorachat25500@gmail.com>
2025-06-26 23:22:34 +0900

    Implement CardHandle class and reconstruct generate_flashcard method in MathFlahscard

--- math_flashcard.py
+++ math_flashcard.py
@@ -2,6 +2,75 @@ import numpy as np
 import tkinter as tk
 import tkinter.font as tkfont
 
+class CardHandle:
+    def __init__(self, frame, difficulty_level=0):
+        self.frame = frame
+
+        self.difficulty_level = difficulty_level
+
+        self.num1:int
+        self.num2:int
+
+        self.op = ["+", "-", "×", "÷"]
+        self.op_index:int
+
+        self.correct_ans:int
+        self.is_correct = False
+
+    def _generate_number(self)->int:
+        return np.random.randint(np.power(10, self.difficulty_level - 1) + 1, np.power(10, self.difficulty_level) + 1)
+    
+    def _calculate_correct_ans(self)->int:
+        #find a correct answer
+        match self.op_index:
+            case 0:
+                return self._compute_plus()
+            case 1:
+                return self._compute_minus()
+            case 2:
+                return self._compute_mul()
+            case 3:
+                return self._compute_div()
+
+    
+    def _compute_plus(self)->int:
+        return self.num1 + self.num2
+    
+    def _compute_minus(self)->int:
+        return self.num1 - self.num2
+    
+    def _compute_mul(self)->int:
+        if len(str(self.num1)) > 1 and len(str(self.num2)) > 1:
+           self.num2 = np.random.randint(1, 10)
+        
+        return self.num1 * self.num2
+    
+    def _compute_div(self)->int:
+        while self.num1 % self.num2 != 0:
+            self.num1 = self._generate_number()
+            self.num2 = self._generate_number()
+        
+        return self.num1 // self.num2
+
+    def generate_card(self):
+        tk.Label(self.frame, text="What is the correct answer of this?").pack(padx=5, pady=30)
+
+        self.op_index = np.random.randint(0, len(self.op))
+        self.num1 = self._generate_number()
+        self.num2 = self._generate_number()
+
+        self.correct_ans = self._calculate_correct_ans()
+
+        tk.Label(self.frame, text=f"{self.num1} {self.op[self.op_index]} {self.num2} = ").pack()
+
+    def handle_correct(self):
+        self.is_correct = True
+        print(f"The answer is {self.correct_ans}! How smart are you!")
+
+    def __str__(self):
+        return f"{self.num1} {self.op[self.op_index]} {self.num2}"
+
+
 class MathFlashcard(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -28,9 +97,10 @@ class MathFlashcard(tk.Tk):
 
         self.is_flashcard_started = False
 
-        self.menu_page()
+        self.cards:list[CardHandle] = []
+        self.current_card:CardHandle
 
-        
+        self.menu_page()
     
     def menu_page(self):
         #create a menu label
@@ -68,32 +138,14 @@ class MathFlashcard(tk.Tk):
     def generate_flashcard(self, difficulty_level):
         self.reload_page()
 
-        #create a flashcard label
-        tk.Label(self.frame, text="What is the correct answer of this?").pack(padx=5, pady=30) 
+        self.cards.append(CardHandle(self.frame, difficulty_level))
 
-        op = ["+", "-", "×", "÷"]
-        op_index = np.random.randint(0, len(op))
+        self.current_card = self.cards[len(self.cards) - 1]
 
-        #random 2 numbers for a question
-        num1 = np.random.randint(np.power(10, difficulty_level - 1) + 1, np.power(10, difficulty_level) + 1)
-        num2 = np.random.randint(np.power(10, difficulty_level - 1) + 1, np.power(10, difficulty_level) + 1)
+        self.current_card.generate_card()
+        print(self.current_card)
 
-        #find a correct answer
-        match op_index:
-            case 0:
-                correct_ans = num1 + num2
-            case 1:
-                correct_ans = num1 - num2
-            case 2:
-                correct_ans = num1 * num2
-            case 3:
-                while num1 % num2 != 0:
-                    num1 = np.random.randint(np.power(10, difficulty_level - 1) + 1, np.power(10, difficulty_level) + 1)
-                    num2 = np.random.randint(np.power(10, difficulty_level - 1) + 1, np.power(10, difficulty_level) + 1)
-                correct_ans = num1 // num2
-            
-        #create a flashcard question
-        tk.Label(self.frame, text=f"{num1} {op[op_index]} {num2} = ").pack()
+        correct_ans = self.current_card.correct_ans
 
         #user's answer(in string)
         ans = tk.StringVar()
@@ -119,6 +171,7 @@ class MathFlashcard(tk.Tk):
             return
 
         if ans_value == correct_ans:
+            self.current_card.handle_correct()
             self.generate_flashcard(difficulty_level)
         else:
             self._handle_worng_answer(ans)
